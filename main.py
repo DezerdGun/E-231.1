d2db3f8ff98b5f2f32971398f06d8636e3c351cf
Isroil Burhonov <IsroilB@corp.itstep>
2023-07-12 21:25:37 +0500

    ya sdelal 1 zadaniye

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello")
+# from datetime import datetime
+#
+# current_datetime = datetime.now()
+# print(current_datetime)
+
+
+# from datetime import date
+#
+# current_year = date.today()
+# print(current_year.year)
+
+
+# from datetime import date
+#
+# current_month = date.today()
+# print(current_month.month)
+
+
+# import datetime
+#
+# week_number = datetime.datetime.today().isocalendar()[1]
+# print(week_number)
+
+
