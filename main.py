adec3180acc95897415104633d3ee1f268306047
Назокат Юлдашева <@corp.itstep>
2023-07-12 21:24:37 +0500

    message

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello")
+# print("hello")
+# import time
+# seconds = time.time()
+# print("Secondsv since epoch =", seconds)
+import time
+seconds = 1689177877.1346107
+local_time = time.ctime(seconds)
+print("Local time:", local_time)
+
